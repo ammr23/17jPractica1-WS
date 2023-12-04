27834f2145124a917c48f852fc4a1678ed79956e
ammr23 <montenegroany@gmail.com>
2023-12-04 05:53:19 -0500

    Carpeta backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
-console.log('Hola mundo !!!')
-const express = require('express')
-const app = express()
+//console.log('Hola mundo !!!')
 
-app.get('/', function (req, res) {
+//Crear Servidor web (Express)
+const express = require('express') //Importa el módulo Express
+const cors = require('cors')//Importa el paquete cors
+
+const server = express()//inicializa la instancia de express
+server.use(cors());//habilitar cors para todas las solicitudes
+
+server.get('/', function (req, res) 
+{
   res.send('Hello World')
 })
 
-app.listen(3000)
+server.get('/ping', (req, res) => {
+  res.status(200).json(
+    {
+      message: 'pong'
+    }
+  )
+})
+server.listen(3000) 
